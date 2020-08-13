700f641af7484bb498d58852caffece94f9cd086
Santiago Restrepo <santirstrp@gmail.com>
2020-08-13 10:27:08 -0500

    Liso el menu, aunque se harán unas pruebas

--- js/menu.js
+++ js/menu.js
@@ -1,2 +1,12 @@
-const menu = document.querySelector('.navbar');
-console.log(menu)
+const $menu = document.querySelector('.navbar');
+const $button = document.querySelector('.hamburguer__menu');
+
+$button.addEventListener('click',hideshow);
+
+function hideshow(){
+    if($menu.classList.contains('is-active')){
+        $menu.classList.remove('is-active');
+    }else{
+        $menu.classList.add('is-active');
+    }
+}
