9c7f9f4d0320c386f1c7c8d0e4cf84ec5e1b3a70
Santiago Restrepo <santirstrp@gmail.com>
2020-08-15 17:42:11 -0500

    Se añade botón de cerrar en el menu, cambio de etiquetas en anotherpostorprojects

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,14 @@
 const $menu = document.querySelector('.navbar');
-const $button = document.querySelector('.hamburguer__menu');
+const $button = document.querySelector('.hamburguer__menu > img');
 
 $button.addEventListener('click',hideshow);
 
 function hideshow(){
     if($menu.classList.contains('is-active')){
         $menu.classList.remove('is-active');
+        $button.setAttribute('src', '../imagenes/hamburguer_menu.svg')
     }else{
         $menu.classList.add('is-active');
+        $button.setAttribute('src', '../imagenes/close.svg')
     }
 }
